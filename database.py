45be041667108d24f4c209a09aa7ca6f13341434
vik1vais1-ai <vik1vais1@gmail.com>
2026-04-04 11:07:47 +0600

    Update database.py

--- database.py
+++ database.py
@@ -1,10 +1,25 @@
-﻿from sqlalchemy import create_engine
+import os
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.pool import NullPool
 
-DATABASE_URL = "sqlite:///fit_rpg.db"
+DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///./fit_rpg.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+connect_args = {}
+engine_args = {}
+
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+    engine_args = {"connect_args": connect_args}
+else:
+    engine_args = {
+        "pool_pre_ping": True,
+        "pool_recycle": 3600,
+        "poolclass": NullPool,
+    }
+
+engine = create_engine(DATABASE_URL, **engine_args)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
